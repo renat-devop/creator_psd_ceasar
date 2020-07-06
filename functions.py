1fd0ec2b55f9c7d12b3dc62ed6b8734cb41af47b
Renat <renat.devop@gmail.com>
2020-07-06 23:50:09 +0300

    Functions

--- functions.py
+++ functions.py
@@ -2,12 +2,12 @@ from random import choice
 
 
 def create_password(alphabet):
-    return ''.join([choice(alphabet) for _ in range(12)])
+    return ''.join(choice(alphabet) for _ in range(12))
 
 
 def encrypting(encrypt, key, chars):
-    return ''.join([chars[chars.find(i) + key] if i in chars else i for i in encrypt])
+    return ''.join(chars[chars.find(i) + key] if i in chars else i for i in encrypt)
 
 
 def decrypting(sequence, key, chars):
-    return ''.join([chars[chars.find(i) - key] if i in chars else i for i in sequence])
+    return ''.join(chars[chars.find(i) - key] if i in chars else i for i in sequence)
